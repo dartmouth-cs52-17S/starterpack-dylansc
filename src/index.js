babc8d5d3c08ebc38cdc5d02f04bdd7ad8289af7
Dylan Scandinaro <Dylan.C.Scandinaro.17@dartmouth.edu>
2017-04-13 11:10:15 -0400

    finished sa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+// console.log('starting up!');
+let num = 0;
+
+setInterval(() => {
+  $('#main').html(`Hello, you've been on this page for ${num} seconds.`);
+  console.log('starting up!');
+  num += 1;
+}, 1000);
+
+$('#main').html('Here we go!');
